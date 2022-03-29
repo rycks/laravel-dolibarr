4d37f89df220169e4b5456ba92c14282238d33d2
Eric Seigne <eric.seigne@cap-rel.fr>
2022-03-29 16:19:17 +0200

    first try Setup API part requests with DolibarrSetupDictionaryCountries

---
+++ src/Models/DolibarrSetupDictionaryCountries.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Caprel\Dolibarr\Models;
+
+use Caprel\Dolibarr\Models\DolibarrCommonObject;
+
+class DolibarrSetupDictionaryCountries extends DolibarrCommonObject
+{
+    protected $fillable = [
+        "id",
+        "sortfield",
+        "sortorder",
+        "limit",
+        "page",
+        "filter",
+        "lang",
+        "sqlfilter",
+        "objectlabel"
+    ];
+
+    protected $attributes = [
+        "id" => null,
+        "sortfield" => "",
+        "sortorder" => "",
+        "limit" => "",
+        "page" => "",
+        "filter" => "",
+        "lang" => "",
+        "sqlfilter" => "",
+        "objectlabel" => ""
+    ];
+
+    public function __construct(array $attributes = [])
+    {
+        $attributes['objectlabel'] =  "setup/dictionary/countries";
+        parent::__construct($attributes);
+    }
+}
