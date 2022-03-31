4126ada6873c365bd44b67009b67c690a4ebc4b9
Eric Seigne <eric.seigne@cap-rel.fr>
2022-03-31 23:02:24 +0200

    add user-agent

--- config/dolibarr.php
+++ config/dolibarr.php
@@ -5,7 +5,7 @@ return [
     /*
      * In case of login/pass authentication, please set theses keys
      * Note: you have to use login+password OR api_key
-     * 
+     *
      */
     'login'=> env('DOLIBARR_USER_LOGIN',""),
     'password' => env('DOLIBARR_USER_PASSWORD',""),
@@ -19,13 +19,19 @@ return [
     /*
      * In case of direct API authentication
      * That information is available on dolibarr/user card
-     * 
+     *
      */
     'api_key' => env('DOLIBARR_API_KEY',""),
 
     /*
      * Your dolibarr URI, https://mydolibarr.cap-rel.fr/ for example
-     * 
+     *
      */
     'server_uri' => env('DOLIBARR_SERVER_URI',""),
+
+    /*
+     * You can specify a special user agent 'signature' for your dolibarr
+     *
+     */
+    'user_agent' => env('DOLIBARR_USER_AGENT',""),
 ];
