be165ae4d4fe7fa80bbae8835f3465cb66dca3f8
Eric Seigne <eric.seigne@cap-rel.fr>
2022-03-31 23:12:28 +0200

    activate line (contract only)

--- src/Models/DolibarrContacts.php
+++ src/Models/DolibarrContacts.php
@@ -39,4 +39,28 @@ class DolibarrContacts extends DolibarrCommonObject
         $attributes['objectlabel'] =  "contacts";
         parent::__construct($attributes);
     }
+
+    /**
+     * activateLine for a contract
+     *
+     * @param   [type]  $id          contact id
+     * @param   [type]  $lineID      line ID
+     * @param   [type]  $attributes  attributes array
+     *
+     * @return  []                   [return description]
+     */
+    public function activateLine($id, $lineID, $attributes = [])
+    {
+        // Log::debug("DolibarrCommonObject call VALIDATE on " . $this->objectlabel . "...");
+        $url = $this->objectlabel . '/' . $id . '/lines/' . $lineID . '/activate';
+        $data = json_encode($attributes);
+        // Log::debug("DolibarrCommonObject::POST for " . $this->objectlabel . ", url=$url , data request is " . $data);
+        $result = ($this->CallAPI(
+            "PUT",
+            $url,
+            $data
+        ));
+        return $result;
+    }
+
 }
