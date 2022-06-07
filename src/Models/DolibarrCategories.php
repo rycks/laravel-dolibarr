369b8730331963337bf7e5a657c5fb2a98d45c96
Eric Seigne <eric.seigne@cap-rel.fr>
2022-06-07 22:32:40 +0200

    addCategorieOnObject

--- src/Models/DolibarrCategories.php
+++ src/Models/DolibarrCategories.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Caprel\Dolibarr\Models;
 
 use Caprel\Dolibarr\Models\DolibarrCommonObject;
@@ -30,7 +29,27 @@ class DolibarrCategories extends DolibarrCommonObject
 
     public function __construct(array $attributes = [])
     {
-        $attributes['objectlabel'] =  "categories";
+        $attributes['objectlabel'] = "categories";
         parent::__construct($attributes);
+    }
+
+    /**
+     * add a tag / categorie on a object
+     *
+     * @param   int     $categID     [$categID description]
+     * @param   String  $objectType  [$objectType description]
+     * @param   int     $objectID    [$objectID description]
+     *
+     * @return  [type]               [return description]
+     */
+    public function addCategorieOnObject(int $categID, String $objectType, int $objectID)
+    {
+        $url = $this->objectlabel . '/' . $categID . '/objects/' . $objectType . '/' . $objectID;
+        $result = ($this->CallAPI(
+            "POST",
+            $url,
+            null
+        ));
+        return $result;
     }
 }
